599f8aa5a466e5f577bce2750176b314fe702ab0
Pham Dang Khoa <mr.pdkhoa@gmail.com>
2018-05-16 14:19:36 +0700

    paging list ROLE

--- src/app/main/role/role.component.ts
+++ src/app/main/role/role.component.ts
@@ -8,8 +8,9 @@ import { DataService } from '../../core/services/data.service';
 })
 export class RoleComponent implements OnInit {
   public pageIndex: number = 1;
-  public pageSize: number = 20;
+  public pageSize: number = 10;
   public pageDisplay: number = 10;
+  public totalRow: number;
   public filter: string = '';
   public roles: any[];
 
@@ -23,6 +24,14 @@ export class RoleComponent implements OnInit {
     this._dataService.get('/api/appRole/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
       .subscribe((respone: any) => {
         this.roles = respone.Items;
+        this.pageIndex = respone.PageIndex;
+        this.pageSize = respone.PageSize;
+        this.totalRow = respone.TotalRows;
       });
+  }
+
+  pageChanged(event: any): void {
+    this.pageIndex=event.page;
+    this.loadData();
   }
 }
